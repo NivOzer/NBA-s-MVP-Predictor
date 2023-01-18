528a10f17e46f91d09dd4fc094ae57d818e54d07
Niv Ozer <niv.ozer@yahoo.com>
2023-01-18 11:34:06 +0200

    19.0 Yay! Cleaning the DF

--- MVP-Clean.py
+++ MVP-Clean.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from tabulate import tabulate
 df = pd.read_csv("DataFrame0")
-print(tabulate(df, headers='keys'))
-df = pd.read_csv("DataFrame1")
-print(tabulate(df, headers='keys'))
-df = pd.read_csv("DataFrame2")
-print(tabulate(df, headers='keys'))
+df1 = pd.read_csv("DataFrame1")
+df2 = pd.read_csv("DataFrame2")
+dfs = [df,df1,df2]
+for df in dfs:
+    del df['Unnamed: 0']
+    df.dropna(subset=['Player'], how='all', inplace=True)
+    df.dropna(subset=['Victories in Season'], how='all', inplace=True)
+    df['Won Conference'].fillna(0, inplace=True)
+    df['Is All-Star'].fillna(0, inplace=True)
+    print(tabulate(df, headers='keys'))
 
